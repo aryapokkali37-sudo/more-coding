ed598098f25cc0ab0390aaba49a4200b12775134
aryapokkali37-sudo <aryapokkali37@gmail.com>
2026-04-10 20:39:26 +0530

    logical operators with the cheezy suace

---
+++ logical operators.py
@@ -0,0 +1,21 @@
+#and,or and not operators
+a=10
+b=-10
+c=0
+if a>0 and b>0 and c>0:
+    print("All numbers are positive")
+else:
+    print("one of these is false")
+
+d=20
+e=-20
+if d>0 or e>0:
+    print("one of these is true")
+else:
+    print("both are cheessyy sauce")
+
+f=12890
+if not f:
+    print("f is not the cheessy sauce")
+else:
+    print("f is the cheessy sauce")
